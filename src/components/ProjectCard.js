458322ede5b7f3aaf26eb3eeac606f4baa5f069d
Rohit5464 <rohit.work5464@gmail.com>
2023-03-26 03:15:23 +0530

    logo change

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,7 +8,7 @@ export const ProjectCard = ({ title, description, imgUrl , Url } , index) => {
         <Col size={12} sm={6} md={4}>
             <a href={Url}>
             <div className="proj-imgbx">
-      <img src={imgUrl} />
+      <img src={imgUrl} className="project1"/>
       
         <div className="proj-txtx">
           <h4>{title}</h4>
